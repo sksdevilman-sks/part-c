bbad1404cc74ae11887e6d3861e14b1adc466d95
sksdevilman-sks <sksdevilman@gmail.com>
2025-11-04 23:27:49 +0530

    Create jsp.html

---
+++ jsp.java
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Attendance Portal</title>
+</head>
+<body>
+    <h2>Enter Attendance Details</h2>
+    <form action="AttendanceServlet" method="post">
+        Student ID: <input type="text" name="studentid" required><br><br>
+        Date: <input type="date" name="date" required><br><br>
+        Status:
+        <select name="status">
+            <option value="Present">Present</option>
+            <option value="Absent">Absent</option>
+        </select><br><br>
+        <input type="submit" value="Submit Attendance">
+    </form>
+</body>
+</html>
